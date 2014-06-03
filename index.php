761299420c9f47106c08d38690097ec495129e0d
mydeadpixel <gary@mydeadpixel.co.uk>
2014-06-03 14:10:04 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$con=mysqli_connect("localhost","yourUsername","yourPassword","yourDatabaseName");
+ 
+	if (mysqli_connect_errno()) {
+	  echo "database connection failed";
+	}
+
+
+	if ($_POST['HandshakeKey'] == "wufooHandshakeKey") {
+		$name = mysqli_real_escape_string($con, $_POST['Field1']);
+		$miles = mysqli_real_escape_string($con, $_POST['Field2']);
+		$EntryId = mysqli_real_escape_string($con, $_POST['EntryId']);
+
+		$sql="INSERT INTO totalMilesDB (name, miles, EntryId)
+		VALUES ('$name', '$miles', '$EntryId')";
+		
+		if (!mysqli_query($con,$sql)) { die('Error: ' . mysqli_error($con)); }
+		mysqli_close($con);
+	}
+
+	else {
+		echo 'Ah ah ah, you didnt say teh magic word!';
+	}
+}
+
+?>
